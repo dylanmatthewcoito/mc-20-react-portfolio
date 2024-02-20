80b860a05047e1da3a6df0149cf229dc63df5d9a
Dylan Coito <dylanmatthewcoito@gmail.com>
2024-02-20 15:33:34 -0800

    Restructuring files.

---
+++ Develop/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
